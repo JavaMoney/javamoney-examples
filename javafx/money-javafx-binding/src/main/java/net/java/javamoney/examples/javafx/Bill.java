7d6dbc6c20b84201d6a42c708c361d4ee1b2d444
keilw <werner.keil@gmx.net>
2013-02-10 00:21:55 +0100

    IN PROGRESS - issue JAVAMONEY-30: JavaFX Binding Example https://java.net/jira/browse/JAVAMONEY-30

---
+++ javafx/money-javafx-binding/src/main/java/net/java/javamoney/examples/javafx/Bill.java
@@ -0,0 +1,36 @@
+/*
+ * JSR 354 JavaFX Binding Example
+ */
+package net.java.javamoney.examples.javafx;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
+import javax.money.MonetaryAmount;
+
+/**
+ *
+ * @author Werner
+ */
+public class Bill {
+    
+    // Define the property
+    private DoubleProperty amountDue = new SimpleDoubleProperty();
+    private double doubleValue = 10d;
+    private CurrencyUnit currency =  Monetary.getCurrencyUnitProvider().get("ISO-4217", "DKK");
+    private MonetaryAmount newAmountDue = Monetary.getMonetaryAmountFactory().get(currency, doubleValue);
+            
+    // Define a getter for the property's value
+    public final double getAmountDue(){return amountDue.get();}
+ 
+    public final MonetaryAmount getNewAmountDue() { return newAmountDue;}
+    
+    // Define a setter for the property's value
+    public final void setAmountDue(double value){amountDue.set(value);}
+ 
+     // Define a getter for the property itself
+    public DoubleProperty amountDueProperty() {return amountDue;}
+    
+    
+}
---
+++ javafx/money-javafx-binding/src/main/java/net/java/javamoney/examples/javafx/Main.java
@@ -0,0 +1,48 @@
+package net.java.javamoney.examples.javafx;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.NumberBinding;
+
+/**
+ *
+ * @author Werner Keil
+ */
+public class Main {
+        public static void main(String[] args) {
+ 
+        Bill bill1 = new Bill();
+        Bill bill2 = new Bill();
+        Bill bill3 = new Bill();
+ 
+        NumberBinding total =
+          Bindings.add(bill1.amountDueProperty().add(bill2.amountDueProperty()),
+              bill3.amountDueProperty());
+        total.addListener(new InvalidationListener() {
+ 
+        public void invalidated(Observable o) {
+                System.out.println("The binding is now invalid.");
+            }
+        });
+
+        // First call makes the binding invalid
+        bill1.setAmountDue(200.00);
+
+        // The binding is now invalid
+        bill2.setAmountDue(100.00);
+        bill3.setAmountDue(75.00);
+
+        // Make the binding valid...
+        System.out.println(total.getValue());
+
+        // Make invalid... 
+        bill3.setAmountDue(150.00);
+
+        // Make valid...
+        System.out.println(total.getValue());
+        
+        System.out.println(bill1.getNewAmountDue());
+        System.out.println(bill2.getNewAmountDue().add(bill1.getNewAmountDue()));
+    }
+}

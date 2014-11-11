758e46bcc32db01b5b86d2176e8874850d1b155b
otaviojava <otaviojava@java.net>
2014-11-11 21:09:11 -0200

    add JSF with moneyAPI

---
+++ web/jsf-money/README.md
@@ -0,0 +1,6 @@
+JSF wit MoneyAPI
+========================
+
+Example using JSF with moneyAPI, with two simple samples.
+* The first one is a list of products, when an element was added the table informs the number of elements, the cheaper one, expensive one, the sum and the average of products.
+* The last one it to do exchange rage from some currencies.
---
+++ web/jsf-money/src/main/java/javamoneyexamples/jsf/money/model/User.java
@@ -0,0 +1,72 @@
+package javamoneyexamples.jsf.money.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.money.CurrencyUnit;
+import javax.money.MonetaryAmount;
+import javax.money.MonetaryCurrencies;
+
+import org.javamoney.moneta.FastMoney;
+import org.javamoney.moneta.function.MonetaryFunctions;
+
+public class User implements Serializable {
+
+	private static final CurrencyUnit CURRENCY = MonetaryCurrencies.getCurrency("USD");
+
+	private static final long serialVersionUID = -4619662846488953817L;
+	
+	private List<Product> products = new ArrayList<>();
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void addProduct(Product product) {
+		Product copy = new Product();
+		copy.setMoney(product.getMoney());
+		copy.setName(product.getName());
+		products.add(copy);
+		product.clear();
+	
+	}
+	
+	public MonetaryAmount getSum() {
+
+		return products.stream().map(Product::getMoney)
+				.reduce(MonetaryFunctions.sum())
+				.orElse(FastMoney.of(0, CURRENCY));
+	}
+	
+	public MonetaryAmount getMin() {
+
+		return products.stream().map(Product::getMoney)
+				.reduce(MonetaryFunctions.min())
+				.orElse(FastMoney.of(0, CURRENCY));
+	}
+	
+	
+	public MonetaryAmount getMax() {
+
+		return products.stream().map(Product::getMoney)
+				.reduce(MonetaryFunctions.max())
+				.orElse(FastMoney.of(0, CURRENCY));
+	}
+	
+	public int getCount() {
+		return products.size();
+	}
+	
+	public MonetaryAmount getAverage() {
+		return products.stream().map(Product::getMoney)
+				.collect(MonetaryFunctions.summarizingMonetary(CURRENCY))
+				.getAverage();
+	}
+
+	@Override
+	public String toString() {
+		return products.toString();
+	}
+	
+}

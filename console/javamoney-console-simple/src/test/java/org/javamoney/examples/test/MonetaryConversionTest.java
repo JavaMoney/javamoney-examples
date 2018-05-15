c23f03e3663328b779ae0dc638d99691de34958e
Werner Keil <werner.keil@gmx.net>
2018-05-15 10:46:46 +0200

    31: Release 1.1
    Task-Url: https://github.com/JavaMoney/javamoney-examples/issues/issues/31

--- console/javamoney-console-simple/src/test/java/org/javamoney/examples/test/MonetaryConversionTest.java
+++ console/javamoney-console-simple/src/test/java/org/javamoney/examples/test/MonetaryConversionTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012, 2015, Anatole Tresch, Werner Keil and others by the @author tag.
+ * Copyright (c) 2012, 2018, Anatole Tresch, Werner Keil and others by the @author tag.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -28,7 +28,7 @@ import javax.money.convert.ExchangeRateProvider;
 import javax.money.convert.MonetaryConversions;
 
 import org.javamoney.moneta.Money;
-import org.javamoney.moneta.internal.convert.ECBCurrentRateProvider;
+import org.javamoney.moneta.convert.ecb.ECBCurrentRateProvider;
 import org.javamoney.moneta.spi.CompoundRateProvider;
 import org.junit.Test;
 

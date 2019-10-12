9990d5c7a429469b990be1a0ac86a97f4922f9f5
Werner Keil <werner.keil@gmx.net>
2019-10-12 14:25:58 +0200

    Added string equals check

--- console/javamoney-console-java10/src/main/java/org/javamoney/examples/console/java10/FormattingLocaleDemo.java
+++ console/javamoney-console-java10/src/main/java/org/javamoney/examples/console/java10/FormattingLocaleDemo.java
@@ -14,6 +14,8 @@ import org.javamoney.moneta.Money;
 public class FormattingLocaleDemo {
 
 	public static void main(String[] args) {
+		final String COMPARISON = "EUR 123,456.78";
+			
 		MonetaryAmountFormat format = MonetaryFormats.getAmountFormat(Locale.ROOT);
 
 		MonetaryAmount source = Money.of(123456.78, "EUR");
@@ -22,9 +24,10 @@ public class FormattingLocaleDemo {
 		System.out.println(formatted); // "EUR 123,456.78", space is a 32 on JDK 8, a 160 on JDK 9 and 10
 		System.out.println((int) formatted.toCharArray()[3]); // JDK 8: 32, JDK 9: 160, JDK 10: 160
 
-		MonetaryAmount result = format.parse("EUR 123,456.78"); // Space is char of 32 (standard space)
+		MonetaryAmount result = format.parse(COMPARISON); // Space is char of 32 (standard space)
 		formatted = format.format(result);
 		System.out.println(formatted);
+		System.out.println(COMPARISON.equals(formatted));
 		System.out.println(result.toString());
 	}
 

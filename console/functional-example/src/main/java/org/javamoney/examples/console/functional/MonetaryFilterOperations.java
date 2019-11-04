feb0a2be9d27e97937050619b6692e0d9750654b
Werner Keil <werner.keil@gmx.net>
2019-11-04 11:14:06 +0100

    Reformatted code

--- console/functional-example/src/main/java/org/javamoney/examples/console/functional/MonetaryFilterOperations.java
+++ console/functional-example/src/main/java/org/javamoney/examples/console/functional/MonetaryFilterOperations.java
@@ -1,3 +1,18 @@
+/*
+ * JavaMoney Examples
+ * Copyright 2015-2019, Werner Keil, Anatole Tresch
+ * and individual contributors by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.javamoney.examples.console.functional;
 
 import java.math.BigDecimal;
@@ -29,21 +44,17 @@ public class MonetaryFilterOperations {
 				.filter((MonetaryFunctions.isCurrency(DOLLAR)))
 				.collect(Collectors.toList());
 		List<MonetaryAmount> notEuro = moneys.stream().filter((MonetaryFunctions.isCurrency(EURO))).collect(Collectors.toList());
-//		List<MonetaryAmount> euroOrDollar = moneys.stream().filter((MonetaryFunctions.containsCurrencies(EURO, DOLLAR))).collect(Collectors.toList());
 		List<MonetaryAmount> dollarGreaterOneHundred = moneys.stream().filter((MonetaryFunctions.isCurrency(DOLLAR).and(MonetaryFunctions.isGreaterThan(money)))).collect(Collectors.toList());
 		List<MonetaryAmount> dollarGreaterOneHundredDistinct = moneys.stream().distinct().filter((MonetaryFunctions.isCurrency(DOLLAR).and(MonetaryFunctions.isGreaterThan(money)))).collect(Collectors.toList());
 		List<MonetaryAmount> between = moneys.stream().filter((MonetaryFunctions.isCurrency(DOLLAR).and(MonetaryFunctions.isBetween(min, max)))).collect(Collectors.toList());
 		
 		System.out.println(justDollar);
 		System.out.println(notEuro);
-//		System.out.println(euroOrDollar);
 		System.out.println(dollarGreaterOneHundred);
 		System.out.println(dollarGreaterOneHundredDistinct);
 		System.out.println(between);
-		
 	}
-	
-	
+
 	public static List<MonetaryAmount> getMoneys() {
 		List<MonetaryAmount> moneys = new ArrayList<>();
 		moneys.add(Money.of(120, DOLLAR));
@@ -51,8 +62,7 @@ public class MonetaryFilterOperations {
 		moneys.add(Money.of(80, DOLLAR));
 		moneys.add(Money.of(90, DOLLAR));
 		moneys.add(Money.of(120, DOLLAR));
-		
-		
+
 		moneys.add(Money.of(120, EURO));
 		moneys.add(Money.of(50, EURO));
 		moneys.add(Money.of(80, EURO));
